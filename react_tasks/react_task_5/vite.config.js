619303ee2cbb71ffdbc7836efe0910f4e23ff9b0
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-10-17 02:30:34 +0530

    task 4 and 5

--- react_tasks/react_task_5/vite.config.js
+++ react_tasks/react_task_5/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const baseUrl =
+    mode == "production"
+      ? `https://khwab-41.github.io/Khwab_Vachhani_21BIT0097_Ethnus_MERN/react_tasks/react_task_5/dist/`
+      : "/";
+  return defineConfig({
+    base: baseUrl,
+    plugins: [react()],
+  });
+};
