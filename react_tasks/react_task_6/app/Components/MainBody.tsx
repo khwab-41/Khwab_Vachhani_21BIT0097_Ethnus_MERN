60f7f070a9fe2a5653fd4c704358cf6189e093c4
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-10-17 02:00:40 +0530

    remove old

--- react_tasks/react_task_6/app/Components/MainBody.tsx
+++ react_tasks/react_task_6/app/Components/MainBody.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BorderCircles from "./BorderCircles";
@@ -15,27 +15,27 @@ const MainBody = () => {
     delaySpeed: 1500,
   });
 
-    return (
-      <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden">
-        <BorderCircles />
-        <Image
-            className="relative rounded-full h-48 w-48 object-cover mx-0 mb-8"
-            src={Profile_IMG}
-            alt=""
-            width={600}
-            height={600}
-          />
-        <div className="text-gray-200 z-20">
-          <h2 className="text-back_portfolio-light_green text-sm uppercase pb-2 tracking-[10px]">
-            Web Developer
-          </h2>
-          <h1 className="text-3xl font-semibold xl:text-4xl">
-            <span className="">{text}</span>
-            <Cursor cursorColor="grey-100" />
-          </h1>
-        </div>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden">
+      <BorderCircles />
+      <Image
+        className="relative rounded-full h-48 w-48 object-cover mx-0 mb-8"
+        src={Profile_IMG}
+        alt=""
+        width={600}
+        height={600}
+      />
+      <div className="text-gray-200 z-20">
+        <h2 className="text-back_portfolio-light_green text-sm uppercase pb-2 tracking-[10px]">
+          Web Developer
+        </h2>
+        <h1 className="text-3xl font-semibold xl:text-4xl">
+          <span className="">{text}</span>
+          <Cursor cursorColor="grey-100" />
+        </h1>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default MainBody;
