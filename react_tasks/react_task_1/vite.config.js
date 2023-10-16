ac0d737c0a71754a8d0cc8b2db14a0c06d954479
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-10-17 00:41:13 +0530

    set base according to mode

--- react_tasks/react_task_1/vite.config.js
+++ react_tasks/react_task_1/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const baseUrl =
+    mode == "production"
+      ? `https://khwab-41.github.io/Khwab_Vachhani_21BIT0097_Ethnus_MERN/react_tasks/react_task_1/dist/`
+      : "/";
+
+  console.log(baseUrl);
+
+  return defineConfig({
+    base: baseUrl,
+    plugins: [react()],
+  });
+};
