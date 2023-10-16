ebe12ec49a86e0d11813016717123f5a452fb2d1
Khwab Vachhani <khwabvachhani.code@gmail.com>
2023-10-17 02:01:13 +0530

    rever next changes

--- react_tasks/react_task_6/next.config.js
+++ react_tasks/react_task_6/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {},
-  assetPrefix:
-    process.env.NODE_ENV == "production"
-      ? `https://khwab-41.github.io/Khwab_Vachhani_21BIT0097_Ethnus_MERN/react_tasks/react_task_6/out/`
-      : "/",
 };
 
 module.exports = nextConfig;
